dcec1c5195ac5c48ba2c7fce84861b6204772844
Reena <Reenapatel.qa@gmail.com>
2022-09-25 21:04:48 -0400

    Committed TestNg Priority

---
+++ src/test/java/testng_Priority/Priority1.java
@@ -0,0 +1,51 @@
+package testng_Priority;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Priority1 {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void initialization() {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/patel/Webdriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		driver.get("https://www.saucedemo.com/");
+
+	}
+
+	@Test(priority = 0)
+	public void login() {
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+	}
+
+	@Test(priority = 2)
+	public void titleTest() {
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+	@Test(priority = 1)
+	public void logoTest() {
+		boolean logo = driver.findElement(By.xpath("//div[@class='login_logo']")).isDisplayed();
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+}
